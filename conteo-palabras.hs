f352e43e3a6c9bc7d71861be2b0091e2bdac85a0
mn <jostenriop@gmail.com>
2020-11-01 16:23:17 -0600

    Iterative cycle

--- conteo-palabras.hs
+++ conteo-palabras.hs
@@ -1,42 +1,130 @@
 import Prelude hiding (null, lookup, map, filter)
-import Data.Map.Lazy hiding (sort,map,foldl)
-import Data.Char
-import Data.List (sort,map)
+import qualified Data.Map as Map
+import Data.List (intersperse)
 import System.IO
-import Functions
+    ( Handle,
+      IOMode(ReadMode, ReadWriteMode),
+      hClose,
+      hIsEOF,
+      hGetLine,
+      openFile,
+      hPutStrLn )
+import Monad (when)
+import DataDef (HypMap)
+import Functions ()
 
 -- El Estado es una "Map" (hash String -> Int) que
 -- para cada palabra da el número de veces que la ha encontrado
-type Estado = Map [Char] Int
+type Estado = Map.Map [Char] Int
 
 -- main crea un Estado vacío e invoca a mainloop
 -- el cual recibe el Estado como parámetro
 main :: IO ()
 main = do 
-       mainloop (fromList[])
+       mainloop (Map.fromList [])
 
 -- Ciclo de ejecución:
 --     recibe un Estado
 --     lee un comando
 --     ejecuta un comando que produce un nuevo Estado
 --     se invoca recursivamente con el nuevo Estado.
-mainloop :: Estado -> IO ()
-mainloop estado = do
+mainloop :: HypMap -> IO ()
+mainloop dict = do
   putStr ">> "
   inpStr <- getLine
   let tokens  = words inpStr
-  let comando = tokens!!0
+  let comand = tokens!!0
+  when (isPreffixOf "load" comand)  (do putStrLn ">>> Nombre archivo de entrada: "
+                                       fileName <- getLine
+                                       inh <- openFile fileName ReadMode
+                                       newDict <- loadDict inh ((Map.fromList []),0)
+                                       hClose inh
+                                       putStrLn $ "Diccionario cargado ( " ++ show(snd(newDict)) ++ " palabras cargadas)"
+                                       mainloop (fst(newDict)))
+
+
+
+
+  when (isPreffixOf "show" comand)  (do putStrLn "Diccionario actual: "
+                                                 putStrLn (show (dict))
+                                                 mainloop dict)
+  when (isPreffixOf "ins" comand) (do putStrLn ">>> Nueva entrada del diccionario: "
+                                      newEntryLine <- getLine
+                                      putStrLn ">>> "
+                                      let newEntry = string2DictEntry newEntryLine
+                                      mainloop (Map.insert (fst newEntry) (snd newEntry) dict)
+     "save" -> do
+                  putStrLn ">>> Nombre archivo de salida: "
+                  fileName <- getLine
+                  saveDict fileName dict 
+                  mainloop dict
+     "exit" -> do
+                  putStrLn "Saliendo..."
+     else then do
+                 putStrLn $ "Comando desconocido ("++ comand ++"): '" ++ inpStr ++ "'" 
+                 mainloop dict
+
+-- función que implementa el comando contar_linea
+contar_linea :: [String] -> Estado -> (Estado, String) 
+contar_linea tokens estado = (foldl contar_token estado tokens, "contar_linea" )
+
+contar_token :: Estado -> String -> Estado
+contar_token estado tok = case Map.lookup tok estado of
+                               Nothing -> Map.insert tok 1 estado
+                               Just valor -> Map.insert tok (valor+1) estado
   
-  case comando of
-     "leer" -> do
-               putStrLn ">>> Nombre archivo entrada: "
-               nombreArchivo <- getLine
-               inh <- openFile nombreArchivo ReadMode
-               nuevoestado <- cargar inh estado
-               hClose inh
-               putStrLn $ "Archivo " ++ nombreArchivo ++ " fue cargado"
-               mainloop nuevoestado
-     "guardar" -> do
+-- función que implementa el comando borrar
+cmd_borrar::[String] -> Estado -> (Estado, String)
+cmd_borrar [] estado = (estado, "No se especificó qué borrar")
+cmd_borrar (v:_) estado = if Map.member v estado 
+                               then (Map.delete v estado, v ++ " borrado")
+                               else (estado, v ++ " no aparece")
+
+-- función que maneja un comando desconocido
+cmd_desconocido ::
+      String -> String -> Estado -> (Bool, Estado, String)
+cmd_desconocido cmd comando estado = (False,estado,mensaje)
+  where mensaje = "Comando desconocido ("++ cmd ++"): '" 
+                                         ++ comando ++ "'"
+
+-- función que implementa el comando imp
+cmd_imp :: Estado -> (Estado, String)
+cmd_imp estado = (estado, show estado)
+
+-- función que implementa leer un archivo línea por línea
+-- y contar las palabras de cada línea
+loadDict :: Handle -> (HypMap,Int) -> IO (HypMap, Int)
+loadDict inh dict = do
+      ineof <- hIsEOF inh
+      if ineof then return dict
+               else do 
+                       inpStr <- hGetLine inh
+                       let newEntry = string2DictEntry inpStr 
+                       loadDict inh ((Map.insert (fst newEntry) (snd newEntry) (fst dict)), (snd dict) + 1) 
+
+saveDict:: String -> HypMap -> IO ()
+saveDict fileName dict = writeFile fileName (dict2String (Map.toList dict))
+
+dict2String :: [(String, [String])] -> String
+dict2String dict | (length dict == 0) = [] 
+                 |otherwise = newLine ++ dict2String  (tail dict)
+      where newLine = fst (head dict) ++ " " ++ concat (intersperse "-" (snd (head dict))) ++ "\n"
+
+string2DictEntry::String -> (String, [String])
+string2DictEntry line = ((head separated), divison )
+      where
+            separated = words line
+            divison = words (fmap (\c -> if c=='-' then ' '; else c) (last separated))
+
+-- descargar :: Handle -> [(String,Int)] -> IO ()
+descargar outh [] = return ()
+descargar outh ((k,v):kvs) = do hPutStrLn outh $ k ++ " " ++ (show v)
+                                descargar outh kvs
+
+
+
+{-
+"guardar" -> do
                putStrLn ">>> Nombre archivo salida: "
                nombreArchivo <- getLine
                outh <- openFile nombreArchivo WriteMode
@@ -55,51 +143,5 @@ mainloop estado = do
                  let (nuevoestado, salida) = cmd_imp estado
                  putStrLn salida
                  mainloop nuevoestado
-     "fin" -> do
-                 putStrLn "Saliendo..."
-     _     -> do
-                 putStrLn $ "Comando desconocido ("++ comando ++"): '" ++ inpStr ++ "'" 
-                 mainloop estado
 
--- función que implementa el comando contar_linea
-contar_linea :: [String] -> Estado -> (Estado, String) 
-contar_linea tokens estado = (foldl contar_token estado tokens, "contar_linea" )
-
-contar_token :: Estado -> String -> Estado
-contar_token estado tok = case lookup tok estado of
-                               Nothing -> insert tok 1 estado
-                               Just valor -> insert tok (valor+1) estado
-  
--- función que implementa el comando borrar
-cmd_borrar::[String] -> Estado -> (Estado, String)
-cmd_borrar [] estado = (estado, "No se especificó qué borrar")
-cmd_borrar (v:_) estado = if member v estado 
-                               then (delete v estado, v ++ " borrado")
-                               else (estado, v ++ " no aparece")
-
--- función que maneja un comando desconocido
-cmd_desconocido ::
-      String -> String -> Estado -> (Bool, Estado, String)
-cmd_desconocido cmd comando estado = (False,estado,mensaje)
-  where mensaje = "Comando desconocido ("++ cmd ++"): '" 
-                                         ++ comando ++ "'"
-
--- función que implementa el comando imp
-cmd_imp :: Estado -> (Estado, String)
-cmd_imp estado = (estado, show estado)
-
--- función que implementa leer un archivo línea por línea
--- y contar las palabras de cada línea
-cargar :: Handle -> Estado -> IO Estado
-cargar inh estado = do
-      ineof <- hIsEOF inh
-      if ineof then return estado
-               else do inpStr <- hGetLine inh
-                       let nuevoestado = foldl contar_token estado (words (map toLower inpStr))
-                       cargar inh nuevoestado
-
-
--- descargar :: Handle -> [(String,Int)] -> IO ()
-descargar outh [] = return ()
-descargar outh ((k,v):kvs) = do hPutStrLn outh $ k ++ " " ++ (show v)
-                                descargar outh kvs
+-}
